2d90ad2f01ea6498d293cd3cfb1bc4d7973a0740
carrie <carrie0129@naver.com>
2021-02-04 13:39:25 +0900

    add make_meeting_publishin

---
+++ app/src/main/java/com/carriedo/moimeun/src/MakeMeeting/MakeMeetingActivity.java
@@ -0,0 +1,16 @@
+package com.carriedo.moimeun.src;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.carriedo.moimeun.R;
+
+public class MakeMeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_meeting);
+    }
+}
