3583343c9e7e2b7555c67bb76161e1a6c05a366d
JiYoungDo <carrie990129@gmail.com>
2021-02-24 21:59:51 +0900

    add detail_meeting

--- app/src/main/java/com/carriedo/moimeun/src/DetailMeetingActivity/DetailMeetingActivity.java
+++ app/src/main/java/com/carriedo/moimeun/src/DetailMeetingActivity/DetailMeetingActivity.java
@@ -1,12 +1,21 @@
 package com.carriedo.moimeun.src.DetailMeetingActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 import com.carriedo.moimeun.R;
 
 public class DetailMeetingActivity extends AppCompatActivity {
+
+    ImageView open_drawer_iv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +23,22 @@ public class DetailMeetingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail_meeting);
 
 
+        open_drawer_iv = findViewById(R.id.detail_meeting_iv_menu) ;
+        open_drawer_iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.detail_meeting_drawer) ;
+                if (!drawer.isDrawerOpen(Gravity.RIGHT)) {
+                    drawer.openDrawer(Gravity.RIGHT) ;
+                }
+                if (drawer.isDrawerOpen(Gravity.RIGHT)) {
+                    drawer.closeDrawer(Gravity.RIGHT) ;
+                }
+            }
+        });
+
+
+
     }
+
 }
